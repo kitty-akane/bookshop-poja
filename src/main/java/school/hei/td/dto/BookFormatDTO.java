3fefd3007bdf64a04a62edca496f71e15fd462bd
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-06-08 09:25:36 +0300

    feat: add BookFormatDTO for API responses

---
+++ src/main/java/school/hei/td/dto/BookFormatDTO.java
@@ -0,0 +1,18 @@
+package school.hei.td.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookFormatDTO {
+    private Long id;
+    private Long bookId;
+    private String formatType;
+    private Double price;
+    private Integer stock;
+}
