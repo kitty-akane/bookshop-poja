17d73f7235768519ed29ef6e69edb4e36328faa4
sitraka <sitraka7011@gmail.com>
2026-06-11 01:53:39 +0300

    feat: add missing bookcopy feature

--- src/main/java/school/hei/td/endpoint/rest/controller/health/BookCopyController.java
+++ src/main/java/school/hei/td/endpoint/rest/controller/health/BookCopyController.java
@@ -1,16 +1,34 @@
 package school.hei.td.endpoint.rest.controller.health;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import school.hei.td.dto.BookCopyDTO;
 import school.hei.td.service.BookCopyService;
 
 @RestController
 @RequestMapping("/api/bookcopies")
 public class BookCopyController {
+
   private final BookCopyService service;
 
   public BookCopyController(BookCopyService service) {
     this.service = service;
+  }
+
+  @GetMapping(value = "/bookCopies")
+  public List<BookCopyDTO> bookCopies() {
+    return service.getAllBookCopies();
+  }
+
+  @PostMapping(value = "/bookCopies")
+  public BookCopyDTO bookCopyDTO(@RequestBody BookCopyDTO dto) {
+    return service.createBookCopy(dto);
+  }
+
+  @PutMapping("/bookCopies/{id}")
+  public BookCopyDTO updateBookCopy(@PathVariable Long id, @RequestBody BookCopyDTO dto) {
+    return service.updateBookCopy(id, dto);
   }
 
   @DeleteMapping("/{id}")
--- src/main/java/school/hei/td/service/BookCopyService.java
+++ src/main/java/school/hei/td/service/BookCopyService.java
@@ -1,16 +1,63 @@
 package school.hei.td.service;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import school.hei.td.dto.BookCopyDTO;
+import school.hei.td.entity.Book;
+import school.hei.td.entity.BookCopy;
+import school.hei.td.mapper.BookCopyMapper;
 import school.hei.td.repository.BookCopyRepository;
+import school.hei.td.repository.BookRepository;
 
 @Service
 public class BookCopyService {
+
   private final BookCopyRepository repository;
+  private final BookRepository bookRepository;
 
   public BookCopyService(BookCopyRepository repository) {
     this.repository = repository;
+  }
+
+  public List<BookCopyDTO> getAllBookCopies() {
+    return repository.findAll().stream().map(BookCopyMapper::toDTO).toList();
+  }
+
+  public BookCopyDTO createBookCopy(BookCopyDTO dto) {
+    Book book =
+        repository
+            .findById(dto.getIdBook())
+            .orElseThrow(() -> new RuntimeException("Book not found"));
+    BookCopy bookCopy = BookCopyMapper.toEntity(dto);
+    bookCopy.setBook(book);
+    BookCopy savedBookCopy = repository.save(bookCopy);
+    return BookCopyMapper.toDTO(savedBookCopy);
+  }
+
+  public BookCopyDTO updateBookCopy(Long id, BookCopyDTO dto) {
+    BookCopy existingBookCopy =
+        repository
+            .findById(id)
+            .orElseThrow(() -> new RuntimeException("BookCopy with id: " + id + " not found"));
+
+    Book book =
+        bookRepository
+            .findById(dto.getIdBook())
+            .orElseThrow(
+                () -> new RuntimeException("Book with id: " + dto.getIdBook() + " not found"));
+
+    existingBookCopy.setBarcode(dto.getBarcode());
+    existingBookCopy.setStatus(dto.getStatus());
+    existingBookCopy.setPrice(dto.getPrice());
+    existingBookCopy.setFormat(dto.getFormat());
+    existingBookCopy.setAcquiredDate(dto.getAcquiredDate());
+    existingBookCopy.setBook(book);
+
+    BookCopy updatedBookCopy = repository.save(existingBookCopy);
+
+    return BookCopyMapper.toDTO(updatedBookCopy);
   }
 
   public void deleteById(Long id) {
