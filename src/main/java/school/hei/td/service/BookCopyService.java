bbeac1d58056885f941eef3151603ac69ea407d8
sitraka <sitraka7011@gmail.com>
2026-06-11 08:13:09 +0300

    fix: remove errors

--- src/main/java/school/hei/td/service/BookCopyService.java
+++ src/main/java/school/hei/td/service/BookCopyService.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import school.hei.td.dto.BookCopyDTO;
-import school.hei.td.entity.Book;
-import school.hei.td.entity.BookCopy;
 import school.hei.td.mapper.BookCopyMapper;
 import school.hei.td.repository.BookCopyRepository;
 import school.hei.td.repository.BookRepository;
@@ -15,37 +13,13 @@ import school.hei.td.repository.BookRepository;
 public class BookCopyService {
 
   private final BookCopyRepository repository;
-  private final BookRepository bookRepository;
 
   public BookCopyService(BookCopyRepository repository, BookRepository bookRepository) {
     this.repository = repository;
-    this.bookRepository = bookRepository;
   }
 
   public List<BookCopyDTO> getAllBookCopies() {
     return repository.findAll().stream().map(BookCopyMapper::toDTO).toList();
-  }
-
-  public BookCopyDTO createBookCopy(BookCopyDTO dto) {
-    Book book =
-        bookRepository
-            .findById(dto.getIdBookCopy())
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    BookCopy bookCopy = new BookCopy();
-    bookCopy.setAvailable(dto.getAvailable());
-    bookCopy.setBook(book);
-    BookCopy savedBookCopy = repository.save(bookCopy);
-    return BookCopyMapper.toDTO(savedBookCopy);
-  }
-
-  public BookCopyDTO updateBookCopy(Long id, BookCopyDTO dto) {
-    BookCopy existingBookCopy =
-        repository
-            .findById(id)
-            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    existingBookCopy.setAvailable(dto.getAvailable());
-    BookCopy updatedBookCopy = repository.save(existingBookCopy);
-    return BookCopyMapper.toDTO(updatedBookCopy);
   }
 
   public void deleteById(Long id) {
