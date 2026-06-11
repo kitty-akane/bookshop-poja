b107f540d43620eabfb8d83cabee4a705b9d3c7f
sitraka rakotoarisoa <sitraka7011@gmail.com>
2026-06-11 08:29:53 +0300

    feat: add bookcopy CREATE endpoint
    * feat: add missing bookcopy feature
    
    * fix: BookCopyService errors
    
    * fix: test error
    
    * fix: remove errors
    
    * fix: remove unused endpoints

--- src/main/java/school/hei/td/endpoint/rest/controller/health/BookCopyController.java
+++ src/main/java/school/hei/td/endpoint/rest/controller/health/BookCopyController.java
@@ -1,16 +1,24 @@
 package school.hei.td.endpoint.rest.controller.health;
 
+import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import school.hei.td.dto.BookCopyDTO;
 import school.hei.td.service.BookCopyService;
 
 @RestController
 @RequestMapping("/api/bookcopies")
 public class BookCopyController {
+
   private final BookCopyService service;
 
   public BookCopyController(BookCopyService service) {
     this.service = service;
+  }
+
+  @GetMapping(value = "/bookCopies")
+  public List<BookCopyDTO> bookCopies() {
+    return service.getAllBookCopies();
   }
 
   @DeleteMapping("/{id}")
--- src/main/java/school/hei/td/service/BookCopyService.java
+++ src/main/java/school/hei/td/service/BookCopyService.java
@@ -1,16 +1,25 @@
 package school.hei.td.service;
 
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+import school.hei.td.dto.BookCopyDTO;
+import school.hei.td.mapper.BookCopyMapper;
 import school.hei.td.repository.BookCopyRepository;
+import school.hei.td.repository.BookRepository;
 
 @Service
 public class BookCopyService {
+
   private final BookCopyRepository repository;
 
-  public BookCopyService(BookCopyRepository repository) {
+  public BookCopyService(BookCopyRepository repository, BookRepository bookRepository) {
     this.repository = repository;
+  }
+
+  public List<BookCopyDTO> getAllBookCopies() {
+    return repository.findAll().stream().map(BookCopyMapper::toDTO).toList();
   }
 
   public void deleteById(Long id) {
