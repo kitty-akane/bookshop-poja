dbf6b8a603bcc762bf128617340708d64c995351
TojoniainaSarobidy <hei.tojo.3@gmail.com>
2026-06-22 09:26:16 +0300

    fix: global exception handler (#11)
    * fix: global exception handler
    
    * fix: handle ResponseStatusException in global exception handler
    
    ---------
    
    Co-authored-by: Tojoniaina Randrianasoa <tojoniaina@gmail.com>

---
+++ src/main/java/school/hei/td/exception/GlobalExceptionHandler.java
@@ -0,0 +1,77 @@
+package school.hei.td.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.server.ResponseStatusException;
+import school.hei.td.exception.model.ExceptionBody;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(ApiException.class)
+  public ResponseEntity<ExceptionBody> handleApiException(
+      ApiException apiException, HttpServletRequest request) {
+    return ResponseEntity.status(apiException.getStatus().value())
+        .body(
+            new ExceptionBody(
+                apiException.getStatus().value(),
+                apiException.getStatus().getReasonPhrase(),
+                apiException.getMessage(),
+                request.getRequestURI(),
+                Instant.now()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ExceptionBody> handleValidation(
+      MethodArgumentNotValidException exception, HttpServletRequest request) {
+
+    return ResponseEntity.badRequest()
+        .body(
+            new ExceptionBody(
+                400, "Bad Request", "Validation failed", request.getRequestURI(), Instant.now()));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ExceptionBody> handleTypeMismatch(
+      MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+    return ResponseEntity.badRequest()
+        .body(
+            new ExceptionBody(
+                400,
+                "Bad Request",
+                "Invalid parameter type",
+                request.getRequestURI(),
+                Instant.now()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ExceptionBody> handleException(
+      Exception exception, HttpServletRequest request) {
+    return ResponseEntity.internalServerError()
+        .body(
+            new ExceptionBody(
+                500,
+                "An internal error has occurred",
+                exception.getMessage(),
+                request.getRequestURI(),
+                Instant.now()));
+  }
+
+  @ExceptionHandler(ResponseStatusException.class)
+  public ResponseEntity<ExceptionBody> handleResponseStatusException(
+      ResponseStatusException exception, HttpServletRequest request) {
+
+    return ResponseEntity.status(exception.getStatusCode())
+        .body(
+            new ExceptionBody(
+                exception.getStatusCode().value(),
+                exception.getStatusCode().toString(),
+                exception.getReason(),
+                request.getRequestURI(),
+                Instant.now()));
+  }
+}
