9d1dfded6009b12f5d9e1ff0cbc5353906e5d383
Mandresy Gaëtan RANDRIANANTOANINA <mandresy2020@gmail.com>
2026-07-08 00:02:34 +0300

    feat: add controller tests

--- src/test/java/school/hei/td/endpoint/rest/controller/health/BookStockControllerTest.java
+++ src/test/java/school/hei/td/endpoint/rest/controller/health/BookStockControllerTest.java
@@ -1,67 +1,31 @@
 package school.hei.td.endpoint.rest.controller.health;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.time.LocalDate;
-import java.util.List;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import school.hei.td.entity.BookCopy;
-import school.hei.td.entity.Format;
-import school.hei.td.entity.Status;
-import school.hei.td.entity.dto.BookDTO;
+import school.hei.td.entity.dto.StockDTO;
+import school.hei.td.enums.Format;
 import school.hei.td.exception.GlobalExceptionHandler;
-import school.hei.td.repository.BookCopyRepository;
-import school.hei.td.service.BookCopyService;
-import school.hei.td.service.BookService;
-import school.hei.td.service.StockService;
+import school.hei.td.service.BookStockService;
 
-@WebMvcTest({BookController.class, GlobalExceptionHandler.class})
+@WebMvcTest({BookStockController.class, GlobalExceptionHandler.class})
 class BookStockControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private BookService bookService;
-  @MockBean private BookCopyService bookCopyService;
-  @MockBean private BookCopyRepository bookCopyRepository;
-  @MockBean private StockService stockService;
-
-  private BookDTO lePetitPrince;
-
-  @BeforeEach
-  void setUp() {
-    lePetitPrince =
-        BookDTO.builder()
-            .idBook(1L)
-            .title("Le Petit Prince")
-            .isbn("978-2070612758")
-            .publicationDate(LocalDate.of(1943, 4, 6))
-            .author("Antoine de Saint-Exupéry")
-            .publisher("Gallimard")
-            .language("Français")
-            .category("Conte philosophique")
-            .description("Un jeune prince decouvre les relations humaines.")
-            .build();
-
-    when(stockService.calculateStock(any())).thenCallRealMethod();
-  }
+  @MockBean private BookStockService bookStockService;
 
   @Test
   void getBookStock_shouldReturnAggregatedStock() throws Exception {
-    when(bookService.getBookById(1L)).thenReturn(lePetitPrince);
-    when(bookCopyRepository.findByBook_IdBook(1L))
-        .thenReturn(
-            List.of(
-                BookCopy.builder().status(Status.AVAILABLE).format(Format.SOFT_COVER).build(),
-                BookCopy.builder().status(Status.BORROWED).format(Format.HARD_COVER).build()));
+    when(bookStockService.getStockByBook(1L))
+        .thenReturn(StockDTO.builder().totalQuantity(2).availableQuantity(1).build());
 
     mockMvc
         .perform(get("/books/1/stock"))
@@ -71,12 +35,8 @@ class BookStockControllerTest {
 
   @Test
   void getBookStockByFormat_shouldFilterByFormat() throws Exception {
-    when(bookService.getBookById(1L)).thenReturn(lePetitPrince);
-    when(bookCopyRepository.findByBook_IdBook(1L))
-        .thenReturn(
-            List.of(
-                BookCopy.builder().status(Status.AVAILABLE).format(Format.SOFT_COVER).build(),
-                BookCopy.builder().status(Status.BORROWED).format(Format.HARD_COVER).build()));
+    when(bookStockService.getStockByBookAndFormat(1L, Format.SOFT_COVER))
+        .thenReturn(StockDTO.builder().totalQuantity(1).availableQuantity(1).build());
 
     mockMvc
         .perform(get("/books/1/stock/SOFT_COVER"))
